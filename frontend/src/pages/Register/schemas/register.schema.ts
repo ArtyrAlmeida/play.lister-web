b774bca6258f2162261e74ced4309d7ee739686f
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2025-03-06 13:36:55 -0300

    fix: ajusta routing do app e adiciona estilos e responsividade ao login e cadastro

--- frontend/src/pages/Register/schemas/register.schema.ts
+++ frontend/src/pages/Register/schemas/register.schema.ts
@@ -4,6 +4,7 @@ const registerSchema = z.object({
     email: z.string().email({ message: 'Email inválido' }),
     password: z.string().min(6, { message: 'Senha deve ter no mínimo 6 caracteres' }),
     name: z.string().min(8, { message: 'Nome deve ter no mínimo 8 caracteres' }),
+    image: z.string().url({ message: 'Url inválida' }),
 })
 
 type RegisterFormTypeSchema = z.infer<typeof registerSchema>
